b0af20a607894217905a42ca9cd77dec6af50f4e
Jordan <Jordantay9014@outlook.com>
2023-10-16 13:17:05 -0500

    feat - updates

---
+++ pizza-byte/pizza-byte.api/Migrations/20231011155717_initialv3.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pizza_byte.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialv3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Pizzas");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Pizzas",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
