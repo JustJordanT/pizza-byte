49c96ba07c3dff161c4ee9ee87e5e89e8e7dff1a
Jordan <Jordantay9014@outlook.com>
2023-10-18 01:12:31 -0500

    feat - project clean up

--- pizza-byte/pizza-byte.contracts/pizza-byte/PutPizzaRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace pizza_byte.contracts.pizza_byte;
-
-public record PutPizzaRequest(
-    string Name,
-    List<string> Toppings,
-    string Crust,
-    string Size,
-    decimal Price
-);
